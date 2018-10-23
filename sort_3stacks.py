3de1cabcc594e31cf7a215f8c6da9837e0094ebb
zhali <zhali@qti.qualcomm.com>
2018-10-23 08:28:40 -0700

    added new

--- sort_3stacks.py
+++ sort_3stacks.py
@@ -7,8 +7,8 @@ def sort_stack (stack1):
     stack3 = []
     
     # write your solution here
-    def quicksort (nums, n, stack_lt, stack_ge):
-        print ("input:", nums, n, "lt:", stack_lt, "ge:", stack_ge)
+    def quicksort (nums, n, stack_lt, stack_ge,step):
+        print (step, " input:", nums, n, "lt:", stack_lt, "ge:", stack_ge)
         if n <= 1:
             return
         p = nums.pop()
@@ -23,21 +23,32 @@ def sort_stack (stack1):
             else:
                 stack_lt.append(t)
                 lt += 1
+
+        print(step, " stack_lt ", stack_lt, lt)
+        print(step, " stack_ge ", stack_ge, ge)
+        quicksort(stack_lt, lt, nums, stack_ge, "less")
+        quicksort(stack_ge, ge, stack_lt, nums, "greater")
         
-        quicksort(stack_lt, lt, nums, stack_ge)
-        quicksort(stack_ge, ge, stack_lt, nums)
-        
+        print("after")
+        print(step, " stack_lt ", stack_lt, lt)
+        print(step, " stack_ge ", stack_ge, ge)
         for _ in range(lt):
-            nums.append(stack_lt.pop())
-        for _ in range(ge):
+            stack_ge.append(stack_lt.pop())
+        for _ in range(lt):
             nums.append(stack_ge.pop())
-        print("output ", nums)
+            
+        for _ in range(ge):
+            stack_lt.append(stack_ge.pop())
+        for _ in range(ge):
+            nums.append(stack_lt.pop())
+            
+        print(step, " output ", nums)
     
-    quicksort(stack1, len(stack1), stack2, stack3)
+    quicksort(stack1, len(stack1), stack2, stack3, "origin")
 
 #stack1 = [1, 2, 3, 4]
 #stack1 = [4, 3, 2, 1]
 #stack1 = [2, 1, 4, 3]
-stack1 = [2, 1]
+#stack1 = [2, 1]
 sort_stack(stack1)
 print(stack1)
